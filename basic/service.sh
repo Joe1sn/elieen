e079c954acd3c616b20704e37c4e059e0e3a6d14
Joe1sn <joe1sn23333@gmail.com>
2022-10-10 15:16:11 +0800

    update startup script

--- basic/service.sh
+++ basic/service.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 # Add your startup script
+if [ -n "$FLAG" ]; then
+    echo $FLAG > /home/pwn/flag
+    chown root:pwn /home/pwn/flag
+    chmod 640 /home/pwn/flag
+    export FLAG=not_flag
+    FLAG=not_flag
+fi
+/etc/init.d/xinetd start;
+sleep infinity;
